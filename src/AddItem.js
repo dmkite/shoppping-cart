9af53f60029e3de0b459f66fc2f438e7d7275db5
Dylan <kite.d92@gmail.com>
2019-01-02 15:04:08 -0800

    abstracted out constant string

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,22 +1,22 @@
 import React , { Component } from 'react'
 
+const DEFAULT_OPTION = 'Select an option...'
+
 class AddItem extends Component{
     constructor(props){
         super(props)
         this.state = {
             quantity: 0,
-            product: 'Select an option...',
-            products: props.products,
-            submit: props.addItemToCart
+            product: DEFAULT_OPTION,
         }
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        this.state.submit(this.state.product, Number(this.state.quantity))
+        this.props.addItemToCart(this.state.product, Number(this.state.quantity))
         this.setState({
             quantity:0,
-            product: 'Select an option...'
+            product: DEFAULT_OPTION
         })
     } 
 
@@ -28,20 +28,22 @@ class AddItem extends Component{
     
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="quantity">Quantity</label>
-                    <input type="number" min="1" max="99" id="quantity" name="quantity" onChange={this.handleChange} value={this.state.quantity}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="products">Products</label>
-                    <select className="custom-select custom-select-lg mb-3" id="products" name="product" onChange={this.handleChange} value={this.state.product}>
-                        <option>Select an option...</option>
-                        {this.state.products.map(product => <option key={product.id} value={product.name}>{product.name}</option>)}
-                    </select>
-                </div>
-                <button className="btn btn-primary" type="submit">submit</button>
-            </form>
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="quantity">Quantity</label>
+                        <input type="number" min="1" max="99" id="quantity" name="quantity" onChange={this.handleChange} value={this.state.quantity} style={{width:'100%'}}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="products">Products</label>
+                        <select className="custom-select custom-select-lg mb-3" id="products" name="product" onChange={this.handleChange} value={this.state.product}>
+                            <option>{DEFAULT_OPTION}</option>
+                            {this.props.products.map(product => <option key={product.id} value={product.name}>{product.name}</option>)}
+                        </select>
+                    </div>
+                    <button className="btn btn-primary" type="submit">submit</button>
+                </form>
+            </div>
         )
     }
 }
